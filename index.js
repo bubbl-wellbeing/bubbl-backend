d0633aa97e01992bcf39ae2ccc8b3b56b1e2b3c0
TheTornSkull <61344663+TheTornSkull@users.noreply.github.com>
2022-05-10 21:46:13 +0530

    added post production code to index.js

--- index.js
+++ index.js
@@ -45,3 +45,10 @@ app.use("/api/v2/music", music);
 // Lister
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
